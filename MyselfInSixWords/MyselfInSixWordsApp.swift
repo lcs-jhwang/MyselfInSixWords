723a5badfd8d2dae0db258b9b334a9c654f6caaf
Julien Hwang <jhwang@lcs.on.ca>
2024-12-03 09:20:41 -0500

    Initial Commit

---
+++ MyselfInSixWords/MyselfInSixWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyselfInSixWordsApp.swift
+//  MyselfInSixWords
+//
+//  Created by Julien Hwang on 3/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyselfInSixWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
